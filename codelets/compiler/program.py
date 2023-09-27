8fcf53268ba6d05004aa0946c037f8af9181de1f
Christopher Priebe <chrisp9090@gmail.com>
2023-09-27 11:07:32 -0700

    Greedy memory allocation + instructions at beginning

--- codelets/compiler/program.py
+++ codelets/compiler/program.py
@@ -255,10 +255,14 @@ class CodeletProgram(object):
         # Codelet instructions are a special case, where all instruction are located at the
         # end of the namespace, but we store them separate from the other namespace items
         # to allow extensibility of instruction addresses
-        input_size = self.relocatables.get_input_namespace_size()
+        # input_size = self.relocatables.get_input_namespace_size()
+        # cdlt_uid = self.get_codelet(cdlt_id).cdlt_uid
+        # instr_addr = self.relocatables.get_relocation('INSTR_MEM', cdlt_uid).start
+        # return (input_size + instr_addr)//8
+
         cdlt_uid = self.get_codelet(cdlt_id).cdlt_uid
         instr_addr = self.relocatables.get_relocation('INSTR_MEM', cdlt_uid).start
-        return (input_size + instr_addr)//8
+        return instr_addr // 8
 
     def get_input_operand_offset(self, operand: Operand):
         return self.relocatables.get_relocation_base(operand)
